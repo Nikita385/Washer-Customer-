7abf1de7dcfb5be36d8efb3b01e62d0b6c6aed39
Nikita385 <nikitap.excelsior@gmail.com>
2019-08-01 15:18:38 +0530

    Profile view Changes- commit

---
+++ Washer/SideMenuScreen/ProfileController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileController.swift
+//  Washer
+//
+//  Created by Excelsior Technologies on 01/08/19.
+//  Copyright © 2019 Excelsior Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ProfileController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
