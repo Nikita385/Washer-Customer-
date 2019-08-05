13f2220e9e1a1235009ab4c1aa075d864e889848
Nikita385 <nikitap.excelsior@gmail.com>
2019-08-05 13:15:05 +0530

    commit radio button changes

--- Washer/SideMenuScreen/AddVehicalController.swift
+++ Washer/SideMenuScreen/AddVehicalController.swift
@@ -36,141 +36,85 @@ class AddVehicalController: UIViewController,UITextFieldDelegate
         // Do any additional setup after loading the view.
     }
     
+
     
-    @IBAction func onBtnBlackClicked(_ sender: UIButton)
+    
+
+    @IBAction func onRadioButtonClick(_ sender: UIButton)
     {
         if sender == btnBlack {
-            btnBlack.isSelected = true
-            sender.setImage(UIImage(named: "whitear.png"), for: .normal)
-            btnWhite.setImage(UIImage(named: ""), for: .normal)
-            btnBlue.setImage(UIImage(named: ""), for: .normal)
-            btnBrown.setImage(UIImage(named: ""), for: .normal)
-        }
-        else
-        {
-            btnBlue.isSelected = false
-            btnBrown.isSelected = false
-            btnBrown.isSelected = false
-            btnBlack.backgroundColor = UIColor.black
-        }
-    }
-    
-    @IBAction func onBtnWhiteClicked(_ sender: UIButton)
-    {
-        if sender == btnWhite {
-            btnWhite.isSelected = true
-            sender.setImage(UIImage(named: "whitear.png"), for: .normal)
-            btnBlue.setImage(UIImage(named: ""), for: .normal)
-            btnBlack.setImage(UIImage(named: ""), for: .normal)
-            btnBrown.setImage(UIImage(named: ""), for: .normal)
-        }
-        else
-        {
-            btnBlack.isSelected = false
-            btnBrown.isSelected = false
-            btnBlue.isSelected = false
-            btnWhite.backgroundColor = UIColor.white
-        }
-    }
-    
-    
-    @IBAction func onBtnBlueClicked(_ sender: UIButton)
-    {
-        if sender == btnBlue {
-            btnBlue.isSelected = true
-            sender.setImage(UIImage(named: "whitear.png"), for: .normal)
-            btnWhite.setImage(UIImage(named: ""), for: .normal)
-            btnBlack.setImage(UIImage(named: ""), for: .normal)
-            btnBrown.setImage(UIImage(named: ""), for: .normal)
+           
+            btnBlack.setImage(UIImage(named: "trueSign"), for: UIControl.State.normal)
 
-        }
-        else
-        {
-            btnBlack.isSelected = false
-            btnBrown.isSelected = false
-            btnWhite.isSelected = false
-            btnBlue.backgroundColor = UIColor.blue
-        }
-    }
-    
-    
-    
-    @IBAction func onBtnBrownClicked(_ sender: UIButton)
-    {
-        if sender == btnBrown {
-            btnBrown.isSelected = true
-            sender.setImage(UIImage(named: "whitear.png"), for: .normal)
-            btnWhite.setImage(UIImage(named: ""), for: .normal)
-            btnBlack.setImage(UIImage(named: ""), for: .normal)
-            btnBlue.setImage(UIImage(named: ""), for: .normal)
-        }
-        else
-        {
-            btnBlack.isSelected = false
-            btnWhite.isSelected = false
-            btnBlue.isSelected = false
-            btnBrown.backgroundColor = UIColor.brown
-        }
-    }
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        
-    }
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-//        if textField == txtPin1 {
-//            let maxLength = 2
-//            let currentString: NSString = textField.text! as NSString
-//            let newString: NSString =
-//                currentString.replacingCharacters(in: range, with: string) as NSString
-//            return newString.length <= maxLength
-//        }
-//        else if textField == txtPin2 {
-//            let maxLength = 2
-//            let currentString: NSString = textField.text! as NSString
-//            let newString: NSString =
-//                currentString.replacingCharacters(in: range, with: string) as NSString
-//            return newString.length <= maxLength
-//        }
-//        else if textField == txtPin3 {
-//            let maxLength = 2
-//            let currentString: NSString = textField.text! as NSString
-//            let newString: NSString =
-//                currentString.replacingCharacters(in: range, with: string) as NSString
-//            return newString.length <= maxLength
-//        }
-//        else if textField == txtPin4 {
-//            let maxLength = 4
-//            let currentString: NSString = textField.text! as NSString
-//            let newString: NSString =
-//                currentString.replacingCharacters(in: range, with: string) as NSString
-//            return newString.length <= maxLength
-//        }
-        
-        let text=textField.text
-        let currentString: NSString = textField.text! as NSString
-        let counter = textField.text?.count
-        
-        if counter! == 1 {
+            btnBlue.setImage(nil, for: UIControl.State.normal)
+            btnBrown.setImage(nil, for: UIControl.State.normal)
+            btnWhite.setImage(nil, for: UIControl.State.normal)
+            btnBlack.isSelected = true;
             
-            if textField == txtPin1{
-                txtPin1.resignFirstResponder()
+        }else if sender == btnWhite{
+           
+            btnWhite.setImage(UIImage(named: "trueSign"), for: UIControl.State.normal)
+            
+            btnBlack.setImage(nil, for: UIControl.State.normal)
+            btnBrown.setImage(nil, for: UIControl.State.normal)
+            btnBlue.setImage(nil, for: UIControl.State.normal)
+            btnWhite.isSelected = true;
+            
+        }else if sender == btnBlue{
+      
+            btnBlue.setImage(UIImage(named: "trueSign"), for: UIControl.State.normal)
+            btnBlack.setImage(nil, for: UIControl.State.normal)
+            btnBrown.setImage(nil, for: UIControl.State.normal)
+            btnWhite.setImage(nil, for: UIControl.State.normal)
+            btnBlue.isSelected = true;
+         
+        }else if sender == btnBrown{
+        
+            btnBrown.setImage(UIImage(named: "trueSign"), for: UIControl.State.normal)
+            btnBlack.setImage(nil, for: UIControl.State.normal)
+            btnWhite.setImage(nil, for: UIControl.State.normal)
+            btnBlue.setImage(nil, for: UIControl.State.normal)
+            btnBrown.isSelected = true;
+            
+        }
+    }
+
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+
+        let text = textField.text
+        let start = string.index(string.startIndex, offsetBy: range.location)
+        let end = string.index(start, offsetBy: range.length)
+        let indexRange = start..<end
+       
+
+        let result = (textField.text as NSString?)?.replacingCharacters(in: range, with: string) ?? string
+
+        let newLength = result.count
+        
+        if textField == txtPin1{
+            if newLength == 2 {
+                txtPin1.text = result
                 txtPin2.becomeFirstResponder()
-                
-            } else if textField == txtPin2{
-                txtPin2.resignFirstResponder()
+                return false
+            }
+        } else if textField == txtPin2{
+            if newLength == 2 {
+                txtPin2.text = result
                 txtPin3.becomeFirstResponder()
-                
-            } else if textField == txtPin3{
-                txtPin3.resignFirstResponder()
-                txtPin4.becomeFirstResponder()
-                
+                return false
             }
             
-        }else if counter! == 4
-        {
-            txtPin4.resignFirstResponder()
+        } else if textField == txtPin3{
+            if newLength == 2 {
+                txtPin3.text = result
+                txtPin4.becomeFirstResponder()
+                return false
+                
+            }
         }
-        
-
-        return true
+      
+        return !(result.count>1)
+     
     }
 }
